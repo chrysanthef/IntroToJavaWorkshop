7aebeed4b0935a9f5305759d4cdcd3b5437bd1da
chrysanthef <league@WTS-iMac-11.attlocal.net>
2017-04-14 11:53:27 -0700

    Day5Workshop

--- src/day3/MadlibsOfTheAmazon.java
+++ src/day3/MadlibsOfTheAmazon.java
@@ -9,25 +9,30 @@ public class MadlibsOfTheAmazon {
 		// If you find yourself having to cross a piranha-infested river, here's how to do it...
 		JOptionPane.showMessageDialog(null, "If you find yourself having to cross a piranha-infested river, here's how to do it...");
 String adjective;
+String liquid;
+String bodyPart;
+String verb;
+String place;
+String story;
 		// Get the user to enter an adjective
-JOptionPane.showInputDialog("Enter Adjective:");
+adjective = JOptionPane.showInputDialog("Enter Adjective:");
 		// Get the user to enter a type of liquid
-
+liquid = JOptionPane.showInputDialog("Enter Type of Liquid:");
 		// Get the user to enter a body part
-
+bodyPart = JOptionPane.showInputDialog("Enter Body Part:");
 		// Get the user to enter a verb
-
+verb = JOptionPane.showInputDialog("Enter Verb:");
 		// Get the user to enter a place
-
+place = JOptionPane.showInputDialog("Enter Place:");
 		// Fit the user's words into this sentence, and save it in a String:
-		// Piranhas are more [adjective] during the day, so cross the river at
-		// night. Piranhas are attracted to fresh [type of liquid] and will most
-		// likely take a bite out of your [body part] if you [verb]. Whatever
-		// you do, if you have an open wound, try to find another way to get
-		// back to the [place]. Good luck!
+		story = "Piranhas are more "+adjective+" during the day, so cross the river at "+
+		"night. Piranhas are attracted to fresh "+liquid+" and will most "+
+		"likely take a bite out of your "+bodyPart+" if you "+verb+". Whatever "+
+		"you do, if you have an open wound, try to find another way to get "+
+		"back to the "+place+". Good luck!";
 		
 		// Make a pop-up for the final story. You can use \n to go to the next line.
-		
+		JOptionPane.showMessageDialog(null, story);
 
 	}
 }
--- src/day5/FlamingNinjaStar.java
+++ src/day5/FlamingNinjaStar.java
@@ -17,30 +17,35 @@ public class FlamingNinjaStar {
 		// *15. Make some adjustments to see what other kinds of shapes you can make.
 
 		// 1. Make a new robot, and set it's pen down.
-
+Robot robot = new Robot();
+robot.penDown();
 		// 12. Set the robot speed to 10
-
+robot.setSpeed(10);
 		// 13. Make all the code below repeat 25 times
-
+for (int j = 0; j < 25; j++) {
+	
 			// 2. Turn the robot 1/8 of a circle
-		
+for (int i = 0; i < 360/8; i++) {
+	robot.move(1);
+robot.turn(1);}
 			// 3. Move the robot 64 pixels
-
+robot.move(64);
 			
 			// 4. Turn the robot 40 degrees to the LEFT. (Negative numbers will turn the robot counter-clockwise.)
-			
+			robot.turn(-40);
 			// 5. Move the robot the distance in the variable flameSize
-			
+			robot.move(flameSize);
 			// 6. Turn the robot 170 degrees
-			
+			robot.turn(170);
 			// 7. Move the robot the distance in the variable flameSize (again)
-			
+			robot.move(flameSize);
 			// 8. Turn the robot 64 degrees to the right
-			
+			robot.turn(64);
 			// 9. Move the robot the distance in the variable baseSize
-			
+			robot.move(baseSize);
 			// 10. Check that your shape is the same as Figure 1. This is one arm of the ninja star.
 			// 11. Color your ninja star like Figure 2.
+			robot.setPenColor(150, 10, 100);}
 		
 	}
 
